aa12f6a82e3085e47f49489888f39883d36b8c17
BillyPilgrim12 <jadwizycmarcin@gmail.com>
2021-06-29 21:25:51 +0200

    Basic timer styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import { FaCog } from "react-icons/fa";
 import styles from "./App.module.scss";
 
 import Timer from "../Timer/Timer";
 import Settings from "../Settings/Settings";
+import Btn from "../Btn/Btn";
 
 const App: React.FC = () => {
+  // State
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+  // Methods
+  const toggleIsSettingsOpen = () => {
+    setIsSettingsOpen(prevState => !prevState);
+  }
+
   return (
     <div className={styles.container}>
       <Timer />
-      <Settings />
+      { isSettingsOpen ? <Settings /> : null }
+      <Btn action={toggleIsSettingsOpen} title="Open Settings">
+        <FaCog />
+      </Btn>
     </div>
   )
 }
